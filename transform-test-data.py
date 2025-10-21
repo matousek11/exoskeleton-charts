7e4d0b7d19e40bdd4e903d6099af4208fc4ac404
Lukas Matousek <matoulu1@uhk.cz>
2025-10-21 16:42:57 -0500

    feat: another round of tests

---
+++ transform-test-data.py
@@ -0,0 +1,22 @@
+path = "pid-tests/transformed-data/pid-test-3.txt"
+
+cleaned_data = []
+
+with open(path, "r") as f:
+    for line in f:
+        if line.startswith("PHYSICAL MOVEMENT"):
+            continue
+        line = line.strip()
+        line = line.replace('Target: ', '')
+        line = line.replace('Angle: ', '')
+        line = line.replace('Error: ', '')
+        line = line.replace('Output: ', '')
+        line = line.replace('Time (ms): ', '')
+        line = line.replace(' | ', ';')
+        cleaned_data.append(line)
+
+# write to file
+with open(path, "w") as f:
+    f.write("\n".join(cleaned_data))
+
+print(f"Transformed! File '{path}' was rewritten with transformed data consisting of ({len(cleaned_data)} lines).")
